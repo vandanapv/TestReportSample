7f01f0abcb1c2012daaaefb8ad0bcdf7f56016e9
vandanapv <48316665+vandanapv@users.noreply.github.com>
2019-03-07 16:11:14 +0530

    Add files via upload

---
+++ Test Sample/Test Sample/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "Hello")
+                label1.Text = "World";
+            else
+                label1.Text = "Hello";
+        }
+    }
+}
